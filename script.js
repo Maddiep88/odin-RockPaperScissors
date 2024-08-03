b5bc474328b8b922521878256ac37267f90af4c9
Madeline Pyles <madelinepyles@gmail.com>
2024-08-03 18:22:41 -0500

    Create getPlayerChoice function.

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function getComputerChoice() {
   const choice = options[Math.floor(Math.random() * options.length)];
   return choice;
 }
+
+function getPlayerChoice() {
+  let validatedInput = false;
+  while (validatedInput == false) {
+    const choice = prompt("Rock Paper Scissors");
+    if (choice == null) {
+      continue;
+    }
+    const choiceInLower = choice.toLowerCase();
+    if (options.includes(choiceInLower)) {
+      validatedInput = true;
+      return choiceInLower;
+    }
+  }
+}
